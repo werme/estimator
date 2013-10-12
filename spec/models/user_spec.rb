926ea0222accca06f100a8e3980bec039ef0324f
Olle Werme <olle.werme@gmail.com>
2013-10-12 18:44:17 +0200

    Integrate FactoryGirl into user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,31 +2,33 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com", password: "mypassword") }
-
-  subject { @user }
+  let(:user) do
+    FactoryGirl.build(:user,
+      name: "Example User", 
+      email: "user@example.com", 
+      password: "mypassword"
+    )
+  end
+  
+  subject { user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
 
   it { should be_valid }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
+  context "when name is not present" do
+    before { user.name = " " }
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
+  context "when name is too long" do
+    before { user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
+  context "when email address is already taken" do
+    before { FactoryGirl.create :user, email: user.email }
     it { should_not be_valid }
   end
 end
