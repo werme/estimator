b560f1cf02b9c82f5d17373e9a952b4eb8a82e53
Olle Werme <olle.werme@gmail.com>
2013-10-12 17:07:12 +0200

    Add basic user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :user do
+    email     "example@test.com"
+    password  "password"
+    name      "unicorn"
   end
 end
