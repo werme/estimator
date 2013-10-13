52acbb6e39f9bbd5c489eca0d3d2299bd025fd61
Olle Werme <olle.werme@gmail.com>
2013-10-13 21:04:31 +0200

    Add scenarios to user authentication spec

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -1,16 +1,23 @@
 require "spec_helper"
 
 feature "User authentication" do
+  before { @user = FactoryGirl.create(:user, email: "unicorn@mail.com", password: "password") }
+  let(:user) { @user }
 
-  scenario "User login" do
-    FactoryGirl.create(:user, email: "unicorn@mail.com", password: "password")
-    
-    visit "/users/sign_in"
-
-    fill_in "user[email]", :with => "unicorn@mail.com"
-    fill_in "user[password]", :with => "password"
-    click_button "Sign in"
-
+  scenario "User signs in" do
+    login(user, "password")
     expect(page).to have_text("Signed in successfully.")
+  end
+
+  scenario "User tries to sign in with with an invalid password" do
+    login(user, "invalid password")
+    expect(page).to have_text("Invalid email or password.")
+  end  
+
+  scenario "Logged in User log out" do
+    login(user, "password")
+    visit root_path
+    click_link "Log out"
+    expect(page).to have_text("Signed out successfully.")
   end
 end
