4280dd98bf8c203d820f97a0ab8faecd1e1381fc
Olle Werme <olle.werme@gmail.com>
2013-11-24 22:56:40 +0100

    Oh the consistency

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -6,7 +6,7 @@ class EstimatesController < ApplicationController
   end
 
   def show
-    @estimate = Estimate.find(params[:id])
+    @estimate = Estimate.find params[:id]
     @note = Note.new
     @estimate.tasks.each { |t| unless t.tasks.empty? then t.hours = t.tasks.map(&:hours).inject(0, :+); t.save end }
     @total = @estimate.tasks.map { |t| t.hours * t.rate }.sum
@@ -18,10 +18,10 @@ class EstimatesController < ApplicationController
   end
 
   def create
-    estimate = Estimate.new(estimate_params)
+    estimate = Estimate.new estimate_params
 
     unless params[:default][:project_type_id].empty?
-      project_type = ProjectType.find(params[:default][:project_type_id])
+      project_type = ProjectType.find params[:default][:project_type_id]
       estimate.tasks = project_type.tasks
       estimate.tasks.each { |t| t.rate = project_type.default_rate || 0; t.hours = 0 }
     end
@@ -35,13 +35,13 @@ class EstimatesController < ApplicationController
   end
 
   def edit
-    @estimate = Estimate.find(params[:id])
+    @estimate = Estimate.find params[:id] 
   end
 
   def update
     estimate = Estimate.find params[:id]
 
-    if estimate.update_attributes(estimate_params)
+    if estimate.update_attributes estimate_params
       redirect_to estimate
     else
       flash[:error] = estimate.errors.full_messages.to_sentence
@@ -56,7 +56,7 @@ class EstimatesController < ApplicationController
   end
 
   def add_user
-    @estimate = Estimate.find(params[:estimate_id])
+    @estimate = Estimate.find params[:estimate_id]
   end
 
   def update_users
@@ -64,8 +64,8 @@ class EstimatesController < ApplicationController
     estimate = Estimate.find params[:estimate_id]
 
     unless params[:editor].empty?
-      if User.find_by_email(params[:editor])
-        user = User.find_by_email(params[:editor])
+      if User.find_by_email params[:editor]
+        user = User.find_by_email params[:editor]
         if estimate.user == user
           flash[:error] = "#{user.name} is the author of this project"
         elsif estimate.editors.include? user
