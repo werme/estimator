212df4cc4d64608159ad507b3786d08d02ea2ee5
Olle Werme <olle.werme@gmail.com>
2013-12-21 20:24:44 +0100

    Rename project management spec to estimate management spec

--- spec/features/estimate_management_spec.rb
+++ spec/features/estimate_management_spec.rb
@@ -1,17 +1,19 @@
 require "spec_helper"
 
-feature "Project management" do
+feature "Estimate management" do
 
   before do
-    @user   = FactoryGirl.create(:user, email: "unicorn@mail.com", password: "password")
+    @user     = FactoryGirl.create(:user, email: "unicorn@mail.com", password: "password")
     @estimate = FactoryGirl.create(:estimate, author: @user)
-    @task     = FactoryGirl.create(:task, estimate: @estimate )
+    @task     = FactoryGirl.create(:task, estimate: @estimate)
+    @subtask  = FactoryGirl.create(:task, parent: @task)
     login(@user, "password")
   end
 
-  let(:user) { @user }
+  let(:user)     { @user }
   let(:estimate) { @estimate }
-  let(:task) { @task }
+  let(:task)     { @task }
+  let(:subtask)  { @subtask }
 
   scenario "User creates a new project" do
     visit root_path
@@ -23,21 +25,29 @@ feature "Project management" do
   end
 
   scenario "User deletes a project" do
-    visit estimate_path(estimate.id)
+    visit estimate_path(estimate)
     click_link "Delete this project"
     expect(page).to have_text("Projects")
   end
 
   scenario "User creates a new task for project" do
-    visit estimate_path(estimate.id)
+    visit estimate_path(estimate)
     click_link "Create a new task for this project"
     fill_in "task[name]", with: "My unicorn task"
     click_button "Create Task"
     expect(page).to have_text("My unicorn task")
   end
 
+  scenario "User creates a new sub task for project" do
+    visit estimate_path(estimate)
+    click_link "Split this task into sub tasks"
+    fill_in "task[name]", with: "My unicorn sub task"
+    click_button "Create Task"
+    expect(page).to have_text("My unicorn sub task")
+  end
+
   scenario "User tries to create a new task without a title" do
-    visit estimate_path(estimate.id)
+    visit estimate_path(estimate)
     click_link "Create a new task for this project"
     fill_in "task[name]", with: " "
     click_button "Create Task"
@@ -45,8 +55,11 @@ feature "Project management" do
   end
 
   scenario "User deletes a task" do 
-    visit estimate_path(estimate.id)
+    visit estimate_path(estimate)
+    expect(page).to have_text(task.name)
+    expect(page).to have_text(subtask.name)
     click_link "Delete this task"
     expect(page).not_to have_text(task.name)
+    expect(page).not_to have_text(subtask.name)
   end
 end
