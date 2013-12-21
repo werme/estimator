6298e97fbf7fc9e458916aeed8b1fe4097e5b653
Olle Werme <olle.werme@gmail.com>
2013-12-21 20:17:58 +0100

    Capitalize context description

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,17 +14,17 @@ describe User do
   it { should respond_to :name, :email, :reset_password_token, :created_at }
   it { should be_valid }
 
-  context "when name is not present" do
+  context "When name is not present" do
     before { user.name = " " }
     it { should_not be_valid }
   end
 
-  context "when name is too long" do
+  context "When name is too long" do
     before { user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
-  context "when email address is already taken" do
+  context "When email address is already taken" do
     before { FactoryGirl.create :user, email: user.email }
     it { should_not be_valid }
   end
