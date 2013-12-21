2e1b89e2c584742ffefd362364b7eaafa3700632
Olle Werme <olle.werme@gmail.com>
2013-12-21 14:50:44 +0100

    Add alias for sub tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :estimate
   belongs_to :template
   belongs_to :parent, class_name: "Task", foreign_key: "parent_id"
-  has_many :tasks, foreign_key: "parent_id", dependent: :destroy
+  has_many :subtasks, class_name: "Task", foreign_key: "parent_id", dependent: :destroy
 
   auto_strip_attributes :name, :nullify => false, :squish => true
 
@@ -11,8 +11,8 @@ class Task < ActiveRecord::Base
   validates :rate, numericality: { only_integer: true }
 
   def hours
-    unless self.tasks.empty?
-      self.tasks.map(&:hours).inject(0, :+)
+    unless self.subtasks.empty?
+      self.subtasks.map(&:hours).inject(0, :+)
     else
       self.read_attribute(:hours)
     end
