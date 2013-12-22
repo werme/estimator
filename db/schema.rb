53c32da1603150e6f510ea6edeed3aba408a148f
Olle Werme <olle.werme@gmail.com>
2013-12-22 16:05:46 +0100

    Remove default values on task integers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218205338) do
+ActiveRecord::Schema.define(version: 20131222145354) do
 
   create_table "estimates", force: true do |t|
     t.datetime "created_at"
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20131218205338) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"
-    t.integer  "rate",                  default: 0
-    t.integer  "hours",       limit: 5, default: 0
+    t.integer  "rate"
+    t.integer  "hours"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "estimate_id"
