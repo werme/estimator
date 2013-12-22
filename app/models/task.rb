80d34ea9f6d00954937c12ca93369d4995983dba
Olle Werme <olle.werme@gmail.com>
2013-12-22 15:33:28 +0100

    Add amoeba config to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,13 +4,17 @@ class Task < ActiveRecord::Base
   belongs_to :parent, class_name: "Task", foreign_key: "parent_id"
   has_many :subtasks, class_name: "Task", foreign_key: "parent_id", dependent: :destroy
 
-  auto_strip_attributes :name, :nullify => false, :squish => true
+  auto_strip_attributes :name, nullify: false, squish: true
 
   validates :name, presence: true
   validates :hours, numericality: { only_integer: true }
   validates :rate, numericality: { only_integer: true }
 
   delegate :author, to: :project, prefix: false
+
+  amoeba do
+    nullify :template_id
+  end
 
   def project
     if self.parent
@@ -22,7 +26,7 @@ class Task < ActiveRecord::Base
 
   def hours
     unless self.subtasks.empty?
-      self.subtasks.map(&:hours).inject(0, :+)
+      self.subtasks.map(&:hours).inject(0,:+)
     else
       self.read_attribute(:hours)
     end
